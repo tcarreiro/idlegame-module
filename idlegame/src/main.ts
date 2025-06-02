62a7d8fea81952ea3ce8da09cc468b2daf184afd
Thiago Carreiro <thiago.carreiro@hotmail.com>
2025-06-02 15:41:11 -0300

    more config

--- idlegame/src/main.ts
+++ idlegame/src/main.ts
@@ -1,14 +1,23 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import { i18nApplication } from './i18n/i18n';
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import PrimeVue from "primevue/config";
+import Tooltip from "primevue/tooltip";
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
+  .use(PrimeVue)
+  .use(pinia)
+  .use(i18nApplication)
+  .use(router)
+  .directive('tooltip', Tooltip)
 
 app.mount('#app')
